bd9777b0f45386230f2a890a5024b4664b15839e
osamaKu <osama.klaib1981@gmail.com>
2020-09-12 11:18:57 +0300

    new repo

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> {{ config('scart.sub_version') }}
+  </div>
+  <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('scart.homepage') }}">SCart: {{ config('scart.title') }}</a>.</strong> All rights
+  reserved.
+</footer>
